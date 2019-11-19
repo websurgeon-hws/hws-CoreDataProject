5eb7d3a9c94b89ea75b3758130aaad78f9365ea8
Peter Barclay <peter@peter-barclay.com>
2019-11-19 08:33:19 +0000

    Conditional saving of NSManagedObjectContext

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -3,20 +3,16 @@
 //
 
 import SwiftUI
-
-struct Student: Hashable {
-    let name: String
-}
+import CoreData
 
 struct ContentView: View {
-    let students = [
-        Student(name: "Harry Potter"),
-        Student(name: "Hermione Granger")
-    ]
+    @Environment(\.managedObjectContext) var moc
     
     var body: some View {
-        List(students, id: \.self) { student in
-            Text(student.name)
+        Button("Save") {
+            if self.moc.hasChanges {
+                try? self.moc.save()
+            }
         }
     }
 }
